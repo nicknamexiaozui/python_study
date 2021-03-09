9cce11ab1f6aa20323334c4f8645efbb280c70e5
nicknamexiaozui <568337691@qq.com>
2021-03-09 15:40:23 +0800

    pycharm提交

---
+++ niukewang/study_test.py
@@ -0,0 +1,19 @@
+#-- coding: utf-8 --
+#@Time : 2021/3/9 14:45
+#@Author : XXXX
+#@Email : XXXX@qq.com
+#@File : study_test.py
+#@Software: PyCharm
+def is_leap_year(year):
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    return False
+
+
+year = int(input("请输入年份："))
+
+if is_leap_year(year):
+    print('闰年')
+else:
+    print('非闰年')
+print(8%100)
